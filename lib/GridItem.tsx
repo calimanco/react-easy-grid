1250ac3e32985fa3d5ba8a5b182b2d842839cb3f
calimanco <calimanco@qq.com>
2024-05-15 11:03:09 +0800

    fix: item position

--- lib/GridItem.tsx
+++ lib/GridItem.tsx
@@ -69,9 +69,9 @@ function GridItem({ start, end, span = 1, style, className, children }: ICompone
       result.push(startP[0])
       result.push(startP[1])
     }
-    if (endP) {
-      result.push(endP[0] + 1)
-      result.push(endP[1] + 1)
+    if (startP && endP) {
+      result.push(endP[0] <= startP[0] - 1 ? endP[0] : endP[0] + 1)
+      result.push(endP[1] <= startP[1] - 1 ? endP[1] : endP[1] + 1)
     }
     return {
       ...contextStyle,
