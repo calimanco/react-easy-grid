f5b2612edf0551087d6c0a650bdfe474f314faff
calimanco <calimanco@qq.com>
2024-06-02 17:50:33 +0800

    build: lint

--- test/eslint.config.js
+++ test/eslint.config.js
@@ -1,7 +1,22 @@
 import base from './../eslint.config.js'
+import globals from 'globals'
 
 export default [
   ...base,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json', './../tsconfig.node.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    }
+  },
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
