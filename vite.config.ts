9740152d13f4649bc7db54b100dd43bf6dc2374b
calimanco <calimanco@qq.com>
2024-06-12 16:47:48 +0800

    feat: add types

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import pkg from './package.json'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({
+    include: ['lib', '"vite-env.d.ts"'],
+    outDir: 'dist/types',
+  })],
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
